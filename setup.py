d03580f03ee00dcc02cb0a528e4eeda7dbdb2899
yegoryakubovich <personal@yegoryakubovich.com>
2024-03-05 11:33:54 +0100

    Version update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, PEP420PackageFinder
 
 
 NAME = 'hg_api_client'
-VERSION = '0.1'
+VERSION = '0.1.1'
 DESCRIPTION = ''
 URL = 'https://yegoryakubovich.com'
 PACKAGE_ROOT = path.abspath(path.dirname(__file__))
